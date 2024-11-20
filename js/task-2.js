12d449d6488df51844f9fa429e4fe9aca33fde9a
Beyza Özdemir <beyzazdmr@hotmail.com>
2024-11-21 00:58:13 +0300

    prettier

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
-function makeArray (firstArray, secondArray, maxLength){
-    const newArray = firstArray.concat(secondArray);
-    return newArray.length > maxLength ? newArray.slice(0, maxLength) : newArray;
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray);
+  return newArray.length > maxLength ? newArray.slice(0, maxLength) : newArray;
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
